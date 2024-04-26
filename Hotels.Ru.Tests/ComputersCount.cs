36f05d0e00bd4753cfb242cb3ced19e84eb09cbd
Сергей Винчестер <creepmah@gmail.com>
2024-04-26 16:48:48 +0300

    Добавьте файлы проекта.

---
+++ Hotels.Ru.Tests/ComputersCount.cs
@@ -0,0 +1,47 @@
+﻿namespace Hotels.Ru.Tests
+{
+    public static class ComputersCount
+    {
+        public static string GetComputersCountName(int count)
+        {
+            int lastNumber = Convert.ToInt32(count.ToString()[^1].ToString());
+            string numberEnding = GetNumberEnding(lastNumber);
+            return $"{count} компьютер{numberEnding}";
+        }
+
+        private static string GetNumberEnding(int number)
+        {
+            string ending;
+            switch (number)
+            {
+                case 0:
+                case 5:
+                case 6:
+                case 7:
+                case 8:
+                case 9:
+                    {
+                        ending = "ов";
+                        break;
+                    }
+                case 1:
+                    {
+                        ending = "";
+                        break;
+                    }
+                case 2:
+                case 3:
+                case 4:
+                    {
+                        ending = "а";
+                        break;
+                    }
+                default:
+                    {
+                        throw new ArgumentOutOfRangeException(nameof(number), "Аргумент должен быть цифрой от 0 до 9");
+                    }
+            }
+            return ending;
+        }
+    }
+}
---
+++ Hotels.Ru.Tests/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Hotels.Ru.Tests
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание №1
+            string comp28Name = ComputersCount.GetComputersCountName(28);
+            Console.WriteLine(comp28Name);  //"28 компьютеров"
+
+
+        }
+
+        
+    }
+}
