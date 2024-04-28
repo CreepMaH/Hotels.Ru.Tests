1f5d77028a28f4a9591b68cc608e0a1ed54f1f77
Сергей Винчестер <creepmah@gmail.com>
2024-04-28 22:08:36 +0300

    Задание №4: Таблица умножения

---
+++ Hotels.Ru.Tests/MultiplicationTable.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+
+namespace Hotels.Ru.Tests
+{
+    public static class MultiplicationTable
+    {
+        public static string GetTable(int limit)
+        {
+            StringBuilder builder = new();
+            builder.Append(GetHeadersRow(limit));
+
+            for (int i = 1; i <= limit; i++)
+            {
+                string resultRow = GetResultsRow(i, limit);
+                builder.Append($"\r\n{resultRow}");
+            }
+
+            return builder.ToString();
+        }
+
+        private static string GetHeadersRow(int limit)
+        {
+            StringBuilder builder = new(" ");
+
+            for (int i = 1; i <= limit; i++)
+            {
+                builder.Append($" {i}");
+            }
+
+            return builder.ToString();
+        }
+
+        private static string GetResultsRow(int multilplier, int limit)
+        {
+            StringBuilder builder = new($"{multilplier}");
+
+            for (int i = 1; i <= limit; i++)
+            {
+                builder.Append($" {i * multilplier}");
+            }
+
+            return builder.ToString();
+        }
+    }
+}
--- Hotels.Ru.Tests/Program.cs
+++ Hotels.Ru.Tests/Program.cs
@@ -9,18 +9,24 @@ namespace Hotels.Ru.Tests
             //Задание №1
             string comp28Name = ComputersCount.GetComputersCountName(28);
             Console.WriteLine(comp28Name);  //"28 компьютеров"
+            Console.WriteLine();
 
             //Задание №2
             int[] input = [42, 12, 18];
             int[] output = CommonDividers.GetCommonDividers(input);
             Console.WriteLine(output.ToDelimitedString(", "));  //"2, 3, 6"
+            Console.WriteLine();
 
             //Задание №3
             int numBegin = 11, numEnd = 20;
             List<int> simpleNumbers = SimpleNumbers.GetSimpleNumbers(numBegin, numEnd);
             Console.WriteLine(simpleNumbers.ToDelimitedString(", "));  //"11, 13, 17, 19"
-        }
+            Console.WriteLine();
 
-        
+            //Задание №4
+            int limit = 5;
+            string multiplicationTable = MultiplicationTable.GetTable(limit);
+            Console.WriteLine(multiplicationTable);    //Таблица умножения до числа 5
+        }
     }
 }
